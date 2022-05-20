c28c28c6e3c0aa76853b6353520c447bee6be940
Roy <roymayorga10@gmail.com>
2022-05-20 12:51:17 +0800

    update

--- assets/jquery/submit.js
+++ assets/jquery/submit.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
-  
+
     
 
-    $(".submit").click(function(){
-      
+    $(".add_new_input").click(function(){
+      $(".first_name").after("<input class='first_name form-control mt-2' type='text' placeholder='Please type your name'>"); 
+    //   ("<input class='first_name form-control' type='text' placeholder='Please type your name'>").$(".first_name")
     })
+
+
+
 
 })
