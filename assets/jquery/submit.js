7eef7a7d00b5e0b23531e832f21b5a15e47e7d43
Roy <roymayorga10@gmail.com>
2022-05-21 22:48:45 +0800

    update

--- assets/jquery/submit.js
+++ assets/jquery/submit.js
@@ -3,8 +3,23 @@ $(document).ready(function(){
     
 
     $(".add_new_input").click(function(){
-      $(".first_name").after("<input class='first_name form-control mt-2' type='text' placeholder='Please type your name'>"); 
+      $(".input-parent").append("<div class='d-flex parent mt-2 justify-content-center'><input class='value form-control' type='text' placeholder='Please type your name'> <i class=' del bi bi-x-circle-fill text-danger ms-2 mt-2' style='font-size: 20px; cursor: pointer;'></i></div>"); 
     //   ("<input class='first_name form-control' type='text' placeholder='Please type your name'>").$(".first_name")
+    })
+
+
+    $(document).on('click','.del',function(){
+      $(this).parents('.parent').remove();  
+    });
+
+
+    $(".sum").click(function(){
+
+        $(".value").each(function(){
+          let x = parseInt($(this).val()); 
+
+        })
+
     })
 
 
